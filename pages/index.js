5506e981d87acf7d5e2d2fa986482e34a9559bb3
Huong Dang <huongd97@knights.ucf.edu>
2021-02-16 20:47:47 -0500

    some words

--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,24 @@
 import NavButton from "../components/NavButton";
+import styles from "../styles/Index.module.css";
 
 export default function Home() {
     return (
         <div>
             <NavButton />
+            <div className={styles.content}>
+                <h1>Let me introduce myself. . .</h1>
+                <p>
+                    Hi! I'm Huong. I'm an early-career software engineer with
+                    experience building web applications and developing APIs.
+                    Originally from sunny Florida, I now live and work in
+                    Virginia.
+                </p>
+            </div>
+            <div className={styles.content}>
+                <h1>Catch me on. . .</h1>
+                <p>GitHub</p>
+                <p>LinkedIn</p>
+            </div>
         </div>
     );
 }
