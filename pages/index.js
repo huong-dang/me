b2fa8bc8e2ed6ffe059085a137377f9a867e03c1
Huong Dang <huongd97@knights.ucf.edu>
2021-02-18 21:40:00 -0500

    f

--- pages/index.js
+++ pages/index.js
@@ -9,7 +9,7 @@ export default function Home() {
             <NavMenu />
             <Heading text="Welcome ☀️" />
 
-            <Link href="/post/name">
+            <Link href="/post/huong">
                 <div className={styles.content}>
                     <p>
                         Hi! I'm Huong. I'm an early-career software engineer
@@ -19,19 +19,20 @@ export default function Home() {
                     </p>
                 </div>
             </Link>
-
-            <div className={styles.content}>
-                <h2>My current projects</h2>
-                <h3>DearGenie</h3>
-                <p>A wishlist.</p>
-                <h3>BetterChoice</h3>
-                <p>Amazon replacement finder.</p>
-                <h3>huongdang.vercel.app</h3>
-                <p>
-                    This website will be my playground to learn web development,
-                    and it'll always be a work-in-progress.
-                </p>
-            </div>
+            <Link href="/post/projects">
+                <div className={styles.content}>
+                    <h2>My current projects</h2>
+                    <h3>DearGenie</h3>
+                    <p>A wishlist.</p>
+                    <h3>BetterChoice</h3>
+                    <p>Amazon replacement finder.</p>
+                    <h3>huongdang.vercel.app</h3>
+                    <p>
+                        This website will be my playground to learn web
+                        development, and it'll always be a work-in-progress.
+                    </p>
+                </div>
+            </Link>
 
             <div className={styles.content}>
                 <h2>Catch me on. . .</h2>
--- pages/post/[title].js
+++ pages/post/[title].js
@@ -1,11 +1,14 @@
 import NavMenu from "../../components/NavMenu";
 import Heading from "../../components/Heading";
+import { useRouter } from "next/router";
 
 export default function Post() {
+    const router = useRouter();
+    const { title } = router.query;
     return (
         <div>
             <NavMenu />
-            <Heading text="Yeet" />
+            <Heading text={title} />
         </div>
     );
 }
