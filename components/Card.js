68e9be76abdf66fbf0074d8a6eaf0790c0cca800
Huong Dang <huongd97@knights.ucf.edu>
2021-02-21 19:53:01 -0500

    Control the gap between flex-wrap items

--- components/Card.js
+++ components/Card.js
@@ -7,9 +7,9 @@ class Card extends Component {
                     .cardContainer {
                         border-radius: 10px;
                         background-color: var(--pink);
-                        margin-bottom: 5vh;
                         box-shadow: 12px 12px 0 0 var(--dark-grey);
                         max-width: 250px;
+                        margin-bottom: 12px;
                     }
                 `}</style>
                 {this.props.children}
