34de4aee38460b7e43c223ba3b95a37d6b8d2dc2
Huong Dang <huongd97@knights.ucf.edu>
2021-02-23 19:48:34 -0500

    blue is a gorgeous theme too

--- components/NavMenu.js
+++ components/NavMenu.js
@@ -40,6 +40,7 @@ function NavMenu() {
                 .menu-container {
                     display: flex;
                     margin: 12px;
+                    justify-content: flex-end;
                 }
 
                 @media only screen and (max-width: 600px) {
@@ -54,94 +55,19 @@ function NavMenu() {
                         margin-right: 100px;
                     }
                 }
-
-                .toggle {
-                    width: 20%;
-                    display: flex;
-                    justify-content: flex-end;
-                }
-                .menu-options {
-                    display: flex;
-                    width: 80%;
-                }
-                /*Simple css to style it like a toggle switch*/
-                .theme-switch-wrapper {
-                    display: flex;
-                    align-items: center;
-                }
-                .theme-switch {
-                    display: inline-block;
-                    height: 34px;
-                    position: relative;
-                    width: 60px;
-                }
-
-                .theme-switch input {
-                    display: none;
-                }
-
-                .slider {
-                    background-color: var(--powder-blue);
-                    bottom: 0;
-                    cursor: pointer;
-                    left: 0;
-                    position: absolute;
-                    right: 0;
-                    top: 0;
-                    transition: 0.4s;
-                }
-
-                .slider:before {
-                    background-color: #fff;
-                    bottom: 4px;
-                    content: "";
-                    height: 26px;
-                    left: 4px;
-                    position: absolute;
-                    transition: 0.4s;
-                    width: 26px;
-                }
-
-                input:checked + .slider {
-                    background-color: var(--pink);
-                }
-
-                input:checked + .slider:before {
-                    transform: translateX(26px);
-                }
-
-                .slider.round {
-                    border-radius: 34px;
-                }
-
-                .slider.round:before {
-                    border-radius: 50%;
-                }
             `}</style>
             <Head>
                 <title>Huong 🌻</title>
             </Head>
-            <div className="menu-options">
-                {options.map((pagePath, idx) => {
-                    return (
-                        <div key={idx}>
-                            <Link href={pagePath}>
-                                <div className="button">
-                                    {navOptions[pagePath]}
-                                </div>
-                            </Link>
-                        </div>
-                    );
-                })}
-            </div>
-            <div className="toggle">
-                <div className="theme-switch-wrapper">
-                    <label className="theme-switch" htmlFor="checkbox">
-                        <input type="checkbox" id="checkbox" />
-                        <div className="slider round"></div>
-                    </label>
-                </div>
-            </div>
+            {options.map((pagePath, idx) => {
+                return (
+                    <div key={idx}>
+                        <Link href={pagePath}>
+                            <div className="button">{navOptions[pagePath]}</div>
+                        </Link>
+                    </div>
+                );
+            })}
         </div>
     );
 }
