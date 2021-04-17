760a490a85dbf8ab924e36b7e6a6dc3a772a99cd
Huong Dang <huongd97@knights.ucf.edu>
2021-04-17 15:38:16 -0400

    we have google analytics!!!!

---
+++ pages/_document.js
@@ -0,0 +1,28 @@
+import Document, { Html, Head, Main, NextScript } from "next/document";
+
+class MyDocument extends Document {
+    static async getInitialProps(ctx) {
+        const initialProps = await Document.getInitialProps(ctx);
+        return { ...initialProps };
+    }
+
+    render() {
+        return (
+            <Html lang="en">
+                <Head>
+                    <script
+                        async
+                        src={`https://www.googletagmanager.com/gtag/js?id=${process.env.gtag}}`}
+                    ></script>
+                    <script src="/googleAnalytics.js" async></script>
+                </Head>
+                <body>
+                    <Main />
+                    <NextScript />
+                </body>
+            </Html>
+        );
+    }
+}
+
+export default MyDocument;
