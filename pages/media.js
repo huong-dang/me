5db836961ca958c18a24a818ef29e4efc04a1bce
Huong Dang <huongd97@knights.ucf.edu>
2021-04-17 12:49:32 -0400

    remove unused css

--- pages/media.js
+++ pages/media.js
@@ -110,80 +110,10 @@ export default function Media() {
             <div className="container">{myMedia}</div>
             <Trailer />
             <style jsx>{`
-                .pill {
-                    border-style: solid;
-                    border-radius: 20px;
-                    border-width: thin;
-                    display: flex;
-                    font-size: 12px;
-                    color: black;
-                    cursor: default;
-                    align-items: center;
-                    padding: 3px 5px 3px 5px;
-                    transition: background-color 0.5s ease;
-                    background-color: var(--powder-blue);
-                    margin-right: 5px;
-                    margin-bottom: 5px;
-                }
-
-                .pill:hover {
-                    border-style: solid;
-                    border-width: thin;
-                    color: white;
-                    cursor: pointer;
-                    background-color: var(--electric-blue);
-                }
-                .pill:active {
-                    opacity: 0.5;
-                }
-
                 p {
                     text-align: center;
                     color: black;
                     font-size: 12px;
-                }
-                .filter-bar {
-                    border-radius: 12px;
-                    border-width: thin;
-                    border-style: solid;
-                    color: black;
-                    margin-bottom: 10px;
-                }
-                @media only screen and (max-width: 768px) {
-                    .filter-bar {
-                        margin-left: 15px;
-                        margin-right: 15px;
-                    }
-                }
-
-                .filter-bar-text {
-                    cursor: pointer;
-                    height: 25px;
-                    align-items: center;
-                    padding-left: 10px;
-                    padding-right: 10px;
-                    display: flex;
-                    justify-content: space-between;
-                }
-
-                #filter-name {
-                    width: 80%;
-                }
-                #filter-x {
-                    width: 20%;
-                    text-align: right;
-                }
-                .selected-filters {
-                    display: flex;
-                    flex-wrap: wrap;
-                    justify-content: center;
-                }
-                .bar {
-                    height: 5px;
-                }
-                .bar-box {
-                    margin-bottom: 27px;
-                    margin-right: 5px;
                 }
             `}</style>
         </div>
